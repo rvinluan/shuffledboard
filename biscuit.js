72e3d0c4ca69b20f76eb5b6302e7f2dd45cd0fab
Robert Vinluan <robert.vinluan@gmail.com>
2019-07-14 21:55:44 -0400

    initial commit

---
+++ biscuit.js
@@ -0,0 +1,33 @@
+var Biscuit = function(scene) {
+  this.gameObject = scene.add.ellipse(w/2, h-200, 100, 100,0x6666ff).setInteractive();
+  scene.physics.add.existing(this.gameObject);
+
+  this.gameObject.body.velocity.x = 0;
+  this.gameObject.body.velocity.y = 0;
+  // this.gameObject.body.setMaxSpeed(1000);
+  this.gameObject.body.bounce.x = 0.1;
+  this.gameObject.body.bounce.y = 0.1;
+  this.gameObject.body.allowDrag = true;
+  this.gameObject.body.collideWorldBounds = true;
+
+  this.state = "LOADED";
+
+  this.gameObject.on('pointerdown', function(pointer) {
+    if(this.state == "LOADED") {
+      this.state = "AIMING";
+      this.aimFrom = {
+        x: pointer.x,
+        y: pointer.y
+      }
+      console.log("aiming...");
+    }
+  }.bind(this))
+}
+
+Biscuit.prototype.shoot = function(x, y) {
+  this.state = "LOADED";
+  var vector = new Phaser.Math.Vector2(x - this.aimFrom.x, y - this.aimFrom.y);
+  console.log("shot length: "+vector.length());
+  this.gameObject.body.setVelocity(vector.x*2, vector.y*2);
+  this.gameObject.body.setDrag(100, 100);
+}
